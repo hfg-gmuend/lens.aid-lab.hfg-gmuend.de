053b231667ea60cf5489210f1fb97e4f47cf66f0
Christopher Pietsch <cpietsch@gmail.com>
2025-11-08 08:40:15 +0000

    Add .nojekyll file to build output and configure Svelte adapter paths

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html',
+			precompress: false,
+			strict: false
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH || ''
+		}
+	}
+};
 
 export default config;
